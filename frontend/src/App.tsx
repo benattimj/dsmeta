610e8e6666a560b4cdc1fee72de9aab7a4903da3
Murilo Benatti <benatti1104@gmail.com>
2022-09-06 00:59:57 +0100

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,21 @@
+import NotificationButton from "./componentes/NotificationButton"
+
 function App() {
 
   return(
+    <>
     <h1>Ola Mundo!</h1>
+  <NotificationButton />
+  <NotificationButton />
+  <NotificationButton />
+  <NotificationButton />
+  <NotificationButton />
+
+
+  </>
   )
 }
+
+
 
 export default App
---
+++ frontend/src/componentes/index.tsx
@@ -0,0 +1,19 @@
+function NotificationButton() {
+import icon from"../../assets/img/notification-icon.svg"
+
+import './styles.css'
+
+    return(
+
+<div className="dsmeta-red-btn">
+    <img src={icon}" alt="Notificar"></img>
+
+</div>
+
+    )
+  }
+  
+  
+  
+  export default NotificationButton
+  
