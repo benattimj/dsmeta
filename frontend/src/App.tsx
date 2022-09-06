d7a96c97c8bfb94b74ba804aa8a759ffff29000d
Murilo Benatti <benatti1104@gmail.com>
2022-09-06 03:09:23 +0100

    Atualizado

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,18 @@
 import NotificationButton from "./componentes/NotificationButton"
+import Header from "./componentes/Header"
 
 function App() {
 
   return(
-    <>
-    <h1>Ola Mundo!</h1>
-  <NotificationButton />
-  <NotificationButton />
-  <NotificationButton />
-  <NotificationButton />
-  <NotificationButton />
 
+<>
+<h1>Ola Mundo</h1>
+<NotificationButton />
+<NotificationButton />
+<NotificationButton />
+<NotificationButton />
 
-  </>
+</>
   )
 }
 
