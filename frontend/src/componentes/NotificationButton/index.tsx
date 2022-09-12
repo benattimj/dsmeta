67349376b2b1bc0c9b0816c3c3d761b35f9cf0bb
benatti1104@gmail.com <benatti1104@gmail.com>
2022-09-11 22:16:53 -0300

    Send notification

--- frontend/src/componentes/NotificationButton/index.tsx
+++ frontend/src/componentes/NotificationButton/index.tsx
@@ -1,17 +1,32 @@
 import icon from '../../assets/img/notification-icon.svg';
 import './styles.css';
+import { BASE_URL } from '../../utils/request';
+import axios from 'axios';
 
-function NotificationButton() {
-
-    return (
-
-                    <div className="dsmeta-red-btn">
-                <img src={icon} alt="Notificar" />
-            </div>
+type Props = {
+    saleId: number;
+}
 
 
-            )
+function handleClick(id: number){
+    axios.get(`${BASE_URL}/sales/${id}/notification`)
+    .then(response => {
+            console.log("Sucesso");
+    })
 
-  }
+}
+
+function NotificationButton( {saleId} : Props) {
+
+      return (
+
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
+            <img src={icon} alt="Notificar" />
+        </div>
+
+
+    )
+
+}
 
 export default NotificationButton;
